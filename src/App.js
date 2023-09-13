4603594e4bfe9185b443850d9c8a1c19b13e0e93
Sebastian Nor <sebanor03@gmail.com>
2023-09-13 20:29:59 -0300

    Ayuda react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
 import Landing from './components/Landing';
+import FAQPage from './components/FAQPage';
 
 
 function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<Login/>}></Route>
+      <Route path="/" element={<Login/>} />
       <Route path="/index" element={<Landing/>} />
+      <Route path="/faq" element={<FAQPage />} />
     </Routes>
     </Router>
 
---
+++ src/components/FAQPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FAQSection from './FaqSection';
+import FAQItem from './FaqItem';
+
+
+function FAQPage() {
+  return (
+    <div className="faq-page">
+      <FAQSection />
+      <FAQItem />
+    </div>
+  );
+}
+
+export default FAQPage;
